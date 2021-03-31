f669757c77445af0940c7afd1658a00f2da8b593
William <shalomwill98@gmail.com>
2021-03-31 14:46:51 +0100

    reworked the function for testing the 2 values

--- janken.js
+++ janken.js
@@ -12,12 +12,45 @@ function computerPlay(){
 }
 console.log(computerPlay());
 
-//make a function to set the user's input and return it
-function userplay(selection){
-    selection = selection.toLowerCase();
-    return selection;
-}
-console.log(userplay("ROck"));
+//get the user's input and save it
+const userPlay = prompt("rock, paper or scissors: ").toLowerCase();
+console.log(userPlay);
 
 //make a function to compare the two values
+function playRound(computer, player){
+    if (computer === player){
+        return "It's a tie!"
+    }else if(computer === "rock"){
+        switch(player){
+            case "paper":
+                return "You win! Paper beats rock.";
+                break;
+            case "scissors":
+                return "You lose! rock beats scissors.";
+                break;
+            default: return "Oh no! error"
+        }
+    }else if(computer === "paper"){
+        switch(player){
+            case "scissors":
+                return "You win! scissors beats paper.";
+                break;
+            case "rock":
+                return "You lose! paper beats rock.";
+                break;
+            default: return "Oh no! error"
+        }
+    }else{
+        switch(player){
+            case "rock":
+                return "You win! rock beats scissors.";
+                break;
+            case "paper":
+                return "You lose! scissors beats paper.";
+                break;
+            default: return "Oh no! error"
+        }
+    }
+}
+
 //decide the winner from the comparison)
