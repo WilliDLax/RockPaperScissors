b3a47e5e9ca4015b3ff0ec767be097e90ba52a44
William <shalomwill98@gmail.com>
2021-03-31 14:04:20 +0100

    made a function to get computer's choice made a function to set user's choice in console

--- janken.js
+++ janken.js
@@ -1,2 +1,23 @@
 //checking if script is linked properly
 console.log("First external javascript!");
+
+/* Creating rock paper scissors game 
+between computer and a user */
+
+//make a function to get computer's input and return it
+function computerPlay(){
+    let options = ["rock","paper","scissors"];
+    let choice = options[Math.floor(Math.random() * options.length)];
+    return choice;
+}
+console.log(computerPlay());
+
+//make a function to set the user's input and return it
+function userplay(selection){
+    selection = selection.toLowerCase();
+    return selection;
+}
+console.log(userplay("ROck"));
+
+//make a function to compare the two values
+//decide the winner from the comparison)
